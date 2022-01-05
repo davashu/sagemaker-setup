a5c49960bd2767d1cf290a240c28dbce2a44790c
manfredminimair <32745525+manfredminimair@users.noreply.github.com>
2022-01-05 17:05:17 -0500

    removed conda package install

--- on-start-more.sh
+++ on-start-more.sh
@@ -5,22 +5,24 @@ sudo -u ec2-user -i <<'EOF'
 echo 'git config --global user.name "dascshu"' >> /home/ec2-user/.bashrc
 echo 'git config --global user.email dasc@shu.edu' >> /home/ec2-user/.bashrc
 
+# This seems to slow down start-up a lot:
+# 
 # Note that "base" is special environment name, include it there as well.
-for env in base /home/ec2-user/anaconda3/envs/*; do
-    source /home/ec2-user/anaconda3/bin/activate $(basename "$env")
+# for env in base /home/ec2-user/anaconda3/envs/*; do
+#     source /home/ec2-user/anaconda3/bin/activate $(basename "$env")
+# 
+#     # Installing packages in the Jupyter system environment can affect stability of your SageMaker
+#     # Notebook Instance.  You can remove this check if you'd like to install Jupyter extensions, etc.
+#     if [ $env = 'JupyterSystemEnv' ]; then
+#       continue
+#     fi
 
-    # Installing packages in the Jupyter system environment can affect stability of your SageMaker
-    # Notebook Instance.  You can remove this check if you'd like to install Jupyter extensions, etc.
-    if [ $env = 'JupyterSystemEnv' ]; then
-      continue
-    fi
-
-    # Replace myPackage with the name of the package you want to install.
-    # pip install --upgrade --quiet myPackage
-    pip install --upgrade --quiet PyAthena
-    # You can also perform "conda install" here as well.
-
-    source /home/ec2-user/anaconda3/bin/deactivate
-done
+#     # Replace myPackage with the name of the package you want to install.
+#     # pip install --upgrade --quiet myPackage
+#     pip install --upgrade --quiet PyAthena
+#     # You can also perform "conda install" here as well.
+# 
+#     source /home/ec2-user/anaconda3/bin/deactivate
+# done
 
 EOF
