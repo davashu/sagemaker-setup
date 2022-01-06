9db6c4366e689b2a6bd9c6339aa1ebb2fb84744a
manfredminimair <32745525+manfredminimair@users.noreply.github.com>
2022-01-06 14:49:24 -0500

    more git config

--- on-start-more.sh
+++ on-start-more.sh
@@ -4,6 +4,7 @@ sudo -u ec2-user -i <<'EOF'
 
 echo 'git config --global user.name "dascshu"' >> /home/ec2-user/.bashrc
 echo 'git config --global user.email dasc@shu.edu' >> /home/ec2-user/.bashrc
+echo 'git config pull.rebase false' >> /home/ec2-user/.bashrc
 
 # This will affect only the Jupyter kernel called "conda_python3".
 conda activate python3
