e4e27621759be60e95f2cc1e7bf799ced0f6ea87
manfredminimair <32745525+manfredminimair@users.noreply.github.com>
2022-01-06 14:30:10 -0500

    install PyAthena

--- on-start-more.sh
+++ on-start-more.sh
@@ -5,4 +5,12 @@ sudo -u ec2-user -i <<'EOF'
 echo 'git config --global user.name "dascshu"' >> /home/ec2-user/.bashrc
 echo 'git config --global user.email dasc@shu.edu' >> /home/ec2-user/.bashrc
 
+# This will affect only the Jupyter kernel called "conda_python3".
+conda activate python3
+
+pip install PyAthena
+# You can also perform "conda install" here as well.
+
+conda deactivate
+
 EOF
