23cca6dc9117890e4bb93d42b3b2c47425ef983d
manfredminimair <32745525+manfredminimair@users.noreply.github.com>
2022-01-06 15:52:15 -0500

    switching to new bash config

--- on-start-more.sh
+++ on-start-more.sh
@@ -5,6 +5,7 @@ sudo -u ec2-user -i <<'EOF'
 echo 'git config --global user.name "dascshu"' >> /home/ec2-user/.bashrc
 echo 'git config --global user.email dasc@shu.edu' >> /home/ec2-user/.bashrc
 echo 'git config pull.rebase false' >> /home/ec2-user/.bashrc
+# curl https://raw.githubusercontent.com/davashu/sagemaker-setup/main/bashconfig.sh >> /home/ec2-user/.bashrc
 
 # This will affect only the Jupyter kernel called "conda_python3".
 conda activate python3
